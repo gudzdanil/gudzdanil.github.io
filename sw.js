097f135e56faefb34da2d92a12f6fb0430dd77aa
danilgudz <gudz.danil@gmail.com>
2018-02-19 14:43:45 +0200

    upd

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-navigator.permissions.query({name: 'notifications'}).then((...args) => {
-    debugger
-})
+self.addEventListener('install', e => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    self.clients.claim();
+
+    navigator.permissions.query({name: 'notifications'}).then((...args) => {
+        debugger
+    });
+});
